6243799d81af5a0c914106c94f486bb954ed30d6
M Usman <usmanali730771@gmail.com>
2024-12-11 16:23:55 +0500

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import RegisterPage from '@/components/RegisterPage'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <RegisterPage />
+    </div>
+  )
+}
+
+export default Home
